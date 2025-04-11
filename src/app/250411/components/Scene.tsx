12c0042d9a8509f99dc499ef00f62fef6b3939b2
Hak Lee <soulofark@gmail.com>
2025-04-11 20:42:07 +0900

    250411 - lint

--- src/app/250411/components/Scene.tsx
+++ src/app/250411/components/Scene.tsx
@@ -1,8 +1,7 @@
 'use client';
 
-import { useState, useCallback, useMemo } from 'react';
+import { useState, useMemo } from 'react';
 import { Canvas } from '@react-three/fiber';
-import * as THREE from 'three';
 
 interface Point {
   x: number;
